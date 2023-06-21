2bf162e8ca01f91e0a7d3f096e934f842f0f1568
Dolphintherobot <122895238+Dolphintherobot@users.noreply.github.com>
2023-06-21 08:18:05 -0600

    Created space_invaders class and added create_walls method.

--- game.py
+++ game.py
@@ -15,3 +15,29 @@ class Wall(pygame.sprite.Sprite):
         self.surface.fill(color)
         self.rect = self.surface.get_rect()
         self.rect.x,self.rect.y = pos
+
+
+class Space_invaders:
+
+    def __init__(self) -> None:
+        '''Initialize a game of space invaders'''
+        size = (1000,1000)
+        self.screen_color = (255,255,255)
+        self.screen = pygame.display.set_mode(size)
+        self.score = 0
+        self.lives = 3
+        self.aliens = pygame.sprite.Group()
+        self.walls = pygame.sprite.Group()
+        self.bullets = pygame.sprite.Group()
+        self.player = aln.Player("player.png")
+    
+    def create_walls(self):
+        '''Purpose:create walls for collision detection'''
+        top_wall = Wall(1,1000,(0,0),self.screen_color)
+        left_wall = Wall(1000,1,(0,0),self.screen_color)
+        right_wall = Wall(1000,1,(1000,0),self.screen_color)
+        self.walls.add(top_wall,left_wall,right_wall)
+
+    
+        
+
